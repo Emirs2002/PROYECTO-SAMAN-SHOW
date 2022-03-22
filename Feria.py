81adc9dac11fd9097a1653cac5d04d4b1125adeb
Emily <em.rodriguez@correo.unimet.edu.ve>
2022-03-22 12:31:16 -0400

    Métodos principales establecidos

---
+++ Feria.py
@@ -0,0 +1,21 @@
+from tools import *
+from Bebida import Bebida
+from Alimento import Alimento
+
+class Feria():
+    def __init__(self, food_db):
+        self.__food_db = food_db
+
+    def get_food_db(self):
+        return self.__food_db
+
+    def show_products():
+        pass
+
+    def delete_product():
+        pass
+
+    def search_product():    #nombre, tipo, o rango de precio
+        pass
+
+    
