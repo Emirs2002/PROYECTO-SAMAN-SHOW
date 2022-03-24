f1a02a13a9edbcd4878d7201bebd89b500ba2c73
Emily <em.rodriguez@correo.unimet.edu.ve>
2022-03-24 02:44:44 -0400

    Métodos show añadidos

--- Alimento.py
+++ Alimento.py
@@ -8,3 +8,15 @@ class Alimento(Producto):
 
     def get_presentacion(self):
         return self.__presentacion
+
+    def show_alimento(self):
+        print(f'''-Nombre: {self.get_nombre_producto()}
+                \n-Tipo: Alimento
+                \n-Cantidad: {self.get_cantidad()}
+                \n-Precio: ${self.get_precio()}''')
+        print("")
+        print("-Presentación: ", end="")
+        if self.get_presentacion() == 1:
+            print("Preparación")
+        elif self.get_presentacion() == 2:
+            print("Empaque")
--- Bebida.py
+++ Bebida.py
@@ -4,4 +4,12 @@ class Bebida(Producto):
     def __init__(self, nombre_producto, clasificacion, precio, cantidad):
         super().__init__(nombre_producto, clasificacion, precio, cantidad)
 
-        
+    def show_bebida(self):
+        print(f'''-Nombre: {self.get_nombre_producto()}
+                \n-Tipo: Bebida
+                \n-Cantidad {self.get_cantidad()}''')
+        print("")
+        print("-Precios:")                
+        print(f"*Pequeño: ${self.get_precio()[0]}")
+        print(f"*Mediano: ${self.get_precio()[1]}")
+        print(f"*Grande: ${self.get_precio()[2]}")
