ddc01df0e228a031187ff2ef4abd872a28a3352c
Emily <em.rodriguez@correo.unimet.edu.ve>
2022-03-29 15:49:11 -0400

    Añadida funciones para enseña

--- Musical.py
+++ Musical.py
@@ -11,5 +11,24 @@ class Musical(Evento):
     def get_num_bandas(self):
         return self.__num_bandas
 
+    def show_musical(self):
+        print(f'''-Nombre: {self.get_nombre_evento()}
+            \n-Fecha: {self.get_fecha()}
+            \n-Tipo: Musical
+            \n-Bandas: {self.get_num_bandas()}''')
+     
+        print("")
+        print("-Cartel:")
+        for cart in range(len(self.get_cartel())):
+            print(f"--> {self.get_cartel()[cart]}")
+
+        print("")
+        print("Asientos:")
+        self.show_asientos()
+
+        print("")
+        print("-Precios:")
+        print(f"*General: ${self.get_precio()[0]}")
+        print(f"*VIP: ${self.get_precio()[1]}")   
     
    
--- Teatro.py
+++ Teatro.py
@@ -10,6 +10,25 @@ class Teatro(Evento):
     def get_sinopsis(self):
         return self.__sinopsis
 
-       
+    def show_teatro(self):
+        print(f'''-Nombre: {self.get_nombre_evento()}
+            \n-Fecha: {self.get_fecha()}
+            \n-Tipo: Obra de teatro
+            \n-Sinopsis: {self.get_sinopsis()}''')
+     
+        print("")
+        print("-Cartel:")
+        for cart in range(len(self.get_cartel())):
+            print(f"--> {self.get_cartel()[cart]}")
+
+        print("")
+        print("Asientos:")
+        self.show_asientos()
+
+        print("")
+        print("-Precios:")
+        print(f"*General: ${self.get_precio()[0]}")
+        print(f"*VIP: ${self.get_precio()[1]}")    
+
 
         
